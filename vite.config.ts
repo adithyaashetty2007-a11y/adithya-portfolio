2a671a10367b4ab77741c706766b28245a558f00
Adithya A Shetty <adithyaashetty2007@gmail.com>
2026-08-13 11:25:04 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -206,6 +206,7 @@ function vitePluginStorageProxy(): Plugin {
 const plugins = [react(), tailwindcss(), jsxLocPlugin(), vitePluginManusRuntime(), vitePluginManusDebugCollector(), vitePluginStorageProxy()];
 
 export default defineConfig({
+  base: '/adithya-portfolio/',
   plugins,
   resolve: {
     alias: {
@@ -239,3 +240,4 @@ export default defineConfig({
     },
   },
 });
+
